11742a06eacb074d110626a4b8fcfbd55c38641c
Michael Moravec <me@majkl.me>
2018-08-15 18:54:06 +0200

    Drop license headers

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * This file is part of the Kdyby (http://www.kdyby.org)
- *
- * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
- *
- * For the full copyright and license information, please view the file license.md that was distributed with this source code.
- */
-
 use Tester\Environment;
 
 if (@!include __DIR__ . '/../vendor/autoload.php') {
